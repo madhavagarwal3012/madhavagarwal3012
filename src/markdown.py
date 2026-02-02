8cbed5a0f4cb23f1b6a33941355e85d59054f6f7
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2026-02-02 14:55:26 +0530

    Update markdown.py

--- src/markdown.py
+++ src/markdown.py
@@ -169,7 +169,7 @@ def generate_captured_table():
     file_path = 'data/captured_data.txt'
     
     if not os.path.exists(file_path) or os.stat(file_path).st_size == 0:
-        return "*No pieces captured yet.*"
+        return "#### No pieces captured yet. \n"
 
     with open(file_path, 'r') as f:
         for line in f:
@@ -189,7 +189,7 @@ def generate_captured_table():
 
     rows = max(len(white_lost), len(black_lost))
     if rows == 0:
-        return "*No pieces captured yet.*"
+        return "#### No pieces captured yet.\n"
     
     # Start the table
     markdown = "\n"
@@ -286,6 +286,7 @@ def board_to_markdown(board):
         markdown += "|   | <span style=\"color:#A78C6F; font-weight:bold;\">A</span> | <span style=\"color:#A78C6F; font-weight:bold;\">B</span> | <span style=\"color:#A78C6F; font-weight:bold;\">C</span> | <span style=\"color:#A78C6F; font-weight:bold;\">D</span> | <span style=\"color:#A78C6F; font-weight:bold;\">E</span> | <span style=\"color:#A78C6F; font-weight:bold;\">F</span> | <span style=\"color:#A78C6F; font-weight:bold;\">G</span> | <span style=\"color:#A78C6F; font-weight:bold;\">H</span> |   |\n"
 
     return markdown
+
 
 
 
